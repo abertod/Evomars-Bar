e8dcccb3ab6cbf9ae30252c8710345c1be951e76
Alberto <albertodamigo@gmail.com>
2024-04-28 21:50:24 +0200

    Texto parpadeante

--- Assets/Scripts/Salvapantallas.cs
+++ Assets/Scripts/Salvapantallas.cs
@@ -12,9 +12,10 @@ public class Salvapantallas : MonoBehaviour
     //Panel opcional de escribir tu nombre
     public GameObject panel_Nombre;
     
+    //variable para texto parpadeante
+    public float timer;
+    public static bool activated = false;
 
-    
-     
 
     // Start is called before the first frame update
     void Start()
@@ -29,6 +30,18 @@ public class Salvapantallas : MonoBehaviour
 
     }
 
+    //Metodo del parpadeo
+    void exit()
+    {
+        if (Input.touchCount > 0 && activated == false)
+        {
+            textoIni.SetActive(false);
+            activated = true;
+        }
+    }
+
+    
+
       // Update is called once per frame
     void Update()
     {
@@ -38,6 +51,19 @@ public class Salvapantallas : MonoBehaviour
         if(Input.anyKeyDown){
             panel_Nombre.SetActive(true);
         }
+
+        //texto parpadeo
+        exit();
+          timer = timer + Time.deltaTime;
+          if(timer >= 0.5)
+          {
+                textoIni.GetComponent<TextMeshProUGUI>().enabled = true;
+          }
+          if(timer >= 2)
+          {
+                textoIni.GetComponent<TextMeshProUGUI>().enabled = false;
+                timer = 0;
+          }
         
     }
 
