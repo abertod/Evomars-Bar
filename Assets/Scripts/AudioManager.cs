e7bf7b1e88b293334a8f7232b29385984ab143db
alberto <albertodamigo@gmail.com>
2024-05-14 13:25:11 +0200

    Pistas de audio en 1 AudioSource. Pruebas de camara

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -11,9 +11,9 @@ public class AudioManager : MonoBehaviour
     public static AudioManager Instance;
 
     //Añade sonidos
-    public AudioClip bandaSonora;
+    //public AudioClip bandaSonora;
     
-    public AudioClip bandaSonora02;
+    //public AudioClip bandaSonora02;
     public AudioClip fxButton;
     AudioSource _audioSource;
 
@@ -21,6 +21,8 @@ public class AudioManager : MonoBehaviour
     public GameObject musicObj2;
     public static AudioSource audioMusic;
     public static AudioSource audioMusic02;
+
+    public AudioClip[] banda;
 
 
     Scene scene;
@@ -42,13 +44,20 @@ public class AudioManager : MonoBehaviour
     {
         
         _audioSource = GetComponent<AudioSource>();
+        
 
         //Aqui se carga la musica 
         audioMusic = musicObj.GetComponent<AudioSource>();
-        audioMusic.clip = bandaSonora;
+        //audioMusic.clip = bandaSonora;
+        audioMusic.clip = banda[0];
         audioMusic.Play();
         audioMusic.loop = true;
         audioMusic.volume = 1f;
+
+
+        audioMusic02 = musicObj2.GetComponent<AudioSource>();
+
+        
         
     }
 
@@ -63,22 +72,34 @@ public class AudioManager : MonoBehaviour
         }
         //Parar musica
         if(Input.GetKeyDown(KeyCode.O)){
-            audioMusic.Stop();
+            audioMusic.Pause();
             audioMusic.loop = false;
         }
 
         scene = SceneManager.GetActiveScene();
 
         if(scene.name == "1Menu"){
-            
-            audioMusic02 = musicObj2.GetComponent<AudioSource>();
+           /* audioMusic.Stop();
+        audioMusic.clip = banda[1];
+        audioMusic.Play();
+        audioMusic.loop = true;
+        audioMusic.volume = 1f;*/
+        audioMusic.Pause();
+        audioMusic.clip = banda[1];
+        audioMusic.Play();
+        
+        
+            /*
             audioMusic02.clip = bandaSonora02;
             audioMusic02.Play();
             audioMusic02.loop = true;
-            audioMusic02.volume = 1f;
+            audioMusic02.volume = 1f;*/
         }
         if(scene.name == "2NivelUno"){
-            audioMusic.Stop();
+            //audioMusic.Stop();
+            audioMusic.Pause();
+        audioMusic.clip = banda[0];
+        audioMusic.Play();
             
         }
 
@@ -117,4 +138,5 @@ public class AudioManager : MonoBehaviour
         _audioSource.PlayOneShot(ac);
     }
 }
+
 
---
+++ Assets/Scripts/Camara.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camara : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         Vector3 mousePos = Input.mousePosition;
+            {
+                Debug.Log("Eje X " + mousePos.x + "Eje Y " + mousePos.y);
+                //Debug.Log("Eje Y " + mousePos.y);
+            }
+    }
+}
