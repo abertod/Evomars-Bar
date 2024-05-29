f97e2fbb876e396267d55cf0f2404b28cc20f16f
alberto <albertodamigo@gmail.com>
2024-05-29 13:38:26 +0200

    Agrandar botellas

--- Assets/Scripts/ObjetosSeleccionables.cs
+++ Assets/Scripts/ObjetosSeleccionables.cs
@@ -2,24 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class ObjetosSeleccionables : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
-    private Color colorOriginal;
+    //private Color colorOriginal;
 
     // Color más transparente al pasar el ratón
-    public Color colorAlPasarElRaton = new Color(1f, 1f, 1f, 0.5f); 
+    //public Color colorAlPasarElRaton = new Color(1f, 1f, 1f, 0.5f); 
+    
+    public float escala;
+    private Vector3 tamañoOriginal;
 
     public float timer;
     public static bool activated = false;
 
     //public Shader shader = new Shader("");
 
+    void Awake(){
+        escala = 1.2f;
+    }
+
     void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        colorOriginal = spriteRenderer.color;
+        //spriteRenderer = GetComponent<SpriteRenderer>();
+        //colorOriginal = spriteRenderer.color;
+
+        tamañoOriginal = transform.localScale;
     }
 
     void OnMouseDown()
@@ -31,15 +41,18 @@ public class ObjetosSeleccionables : MonoBehaviour
     void OnMouseEnter()
     {
         // Cuando el ratón entra en el objeto
-        spriteRenderer.color = colorAlPasarElRaton;
+        //spriteRenderer.color = colorAlPasarElRaton;
+        transform.localScale = tamañoOriginal*escala;
     }
 
     void OnMouseExit()
     {
         // Cuando el ratón sale del objeto
-        spriteRenderer.color = colorOriginal;
+        //spriteRenderer.color = colorOriginal;
+        transform.localScale = tamañoOriginal;
     }
 
+    /*
     //Metodo del parpadeo
     void exit()
     {
@@ -47,10 +60,10 @@ public class ObjetosSeleccionables : MonoBehaviour
         {
             //spriteRenderer.SetActive(false);
             spriteRenderer.flipX = false;
-            spriteRenderer.color = Random.ColorHSV();
+            //spriteRenderer.color = Random.ColorHSV();
             activated = true;
         }
-    }
+    }*/
     
     void Seleccionar()
     {
@@ -62,21 +75,22 @@ public class ObjetosSeleccionables : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        /*
         //Es una prueba. Es para intentar que parpadeen con un brillo para que se sepa que son objetos interactuables
         exit();
           timer = timer + Time.deltaTime;
           if(timer >= 0.5)
           {
                 spriteRenderer.flipX = true;
-                spriteRenderer.color = colorOriginal;
+                //spriteRenderer.color = colorOriginal;
           }
           if(timer >= 2)
           {
                 spriteRenderer.flipX = false;
-                spriteRenderer.color = Random.ColorHSV();
+                //spriteRenderer.color = Random.ColorHSV();
                 timer = 0;
           }
+          */
     }
 
 
