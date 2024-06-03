de35191eeef9094169443411a54381e2207180af
Alberto <albertodamigo@gmail.com>
2024-06-03 23:49:36 +0200

    Desactivar luces cuando se activa minijuego

--- Assets/Scripts/Luces.cs
+++ Assets/Scripts/Luces.cs
@@ -6,7 +6,9 @@ public class Luces : MonoBehaviour
 {
     public static Luces lucesInstance;
     public GameObject luces;
-    //public GameObject minijuego;
+    public GameObject minijuego;
+
+    //public GameObject minijuego2;
     //public bool activado = false;
 
     
@@ -15,15 +17,45 @@ public class Luces : MonoBehaviour
     void Start()
     {
         luces = GameObject.Find("luces");
+        minijuego = GameObject.Find("Minijuego");
+        //minijuego2 = GameObject.Find("Minijuego_2");
     }
+
+    public void DesactivarLuces()
+    {
+        luces.SetActive(false);
+        
+    }
+
+    public void ActivarLuces()
+    {
+        luces.SetActive(true);
+    }
+
+    /*public void DesactivarLuces02()
+    {
+        luces.SetActive(false);
+        
+    }
+
+    public void ActivarLuces02()
+    {
+        luces.SetActive(true);
+    }*/
 
     // Update is called once per frame
     void Update()
     {
-        /*if(ApareceMinijuego.apareceMinijuego.activado==true){
-            luces.SetActive(false);
+        if(minijuego.activeSelf){
+            DesactivarLuces();
         }else{
-            luces.SetActive(true);
+            ActivarLuces();
+        }
+
+        /*if(minijuego2.activeSelf){
+            DesactivarLuces02();
+        }else{
+            ActivarLuces02();
         }*/
 
         
