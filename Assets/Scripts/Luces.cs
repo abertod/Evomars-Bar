fe1cb4bf5bd2f94d574fe3324ed5ca89446b7d64
Alberto <albertodamigo@gmail.com>
2024-06-03 21:46:29 +0200

    configurando pt6

---
+++ Assets/Scripts/Luces.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Luces : MonoBehaviour
+{
+    public GameObject luces;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        luces = GameObject.Find("luces");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
